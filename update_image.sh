591496f4a909370b6b6eb6e8539737b6af820920
ryuta <ryuta0221@gmail.com>
2017-09-14 23:05:12 +0900

    Add: kubectlインストールイメージを作成

--- update_image.sh
+++ update_image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -eux
 
-gcloud container clusters get-credentials sample-gke
+gcloud container clusters get-credentials sample-gke --zone us-east1-d
 kubectl set image deployment/go-sample go-sample=gcr.io/$PROJECT_ID/sample-gke:$REVISION_ID
 kubectl set image deployment/go-sample go-sample-nginx=gcr.io/$PROJECT_ID/sample-gke-nginx:$REVISION_ID
