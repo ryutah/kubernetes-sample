73720d128749a68ed04ea08fd1b19e0f4c2ce21f
ryuta <ryuta0221@gmail.com>
2017-09-14 22:55:19 +0900

    Update: デプロイシェル修正

--- update_image.sh
+++ update_image.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -eux
 
+gcloud container clusters get-credentials sample-gke
 kubectl set image deployment/go-sample go-sample=gcr.io/$PROJECT_ID/sample-gke:$REVISION_ID
 kubectl set image deployment/go-sample go-sample-nginx=gcr.io/$PROJECT_ID/sample-gke-nginx:$REVISION_ID
